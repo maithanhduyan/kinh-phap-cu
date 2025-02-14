1d331682c59ea2ba39368ea0626a75a2145469d7
Mai Thành Duy An <tiachop0102@gmail.com>
2025-02-14 12:01:38 +0900

    Update VuePress configuration to set base URL for production environment

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -13,12 +13,8 @@ export default defineUserConfig({
   description: 'Kinh Pháp Cú',
 
   theme: defaultTheme({
-
+    base: process.env.NODE_ENV === 'production' ? '/kinh-phap-cu/' : '/',
     logo: '/img/icon/buddha_vector_icon.png',
-    repo: 'maithanhduyan/kinh-phap-cu',
-    docsRepo: 'maithanhduyan/kinh-phap-cu',
-    docsDir: 'docs',
-    docsBranch: 'master',
     head: head,
     navbar: navbar,
     sidebar: sidebar,
